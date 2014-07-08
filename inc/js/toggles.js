53b719ef5570901073c6f7f1927377e1f07d1f3e
Maurice van Creij <maurice.vancreij@webqem.com>
2014-07-08 14:14:31 +1000

    Reduced reliance on typography.

--- inc/js/toggles.js
+++
@@ -1,8 +0,0 @@
-/*
-	Combined JavaScripts
-
-	@codekit-append "../src/useful.toggles.js"
-	@codekit-append "../../useful-transitions/src/useful.transitions.js"
-	@codekit-append "../../useful-instances/src/useful.instances.js"
-	@codekit-append "../../useful-polyfills/src/useful.polyfills.js"
-*/
